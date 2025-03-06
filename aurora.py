7c78c39105a3dec4e2e0c78c862cf93a1b28e807
Jonathan Gazeley <me@jonathangazeley.com>
2025-03-06 22:14:27 +0000

    Rework sample data

--- aurora.py
+++ aurora.py
@@ -11,25 +11,8 @@ args = parser.parse_args()
 
 #result = subprocess.run(['aurora', '-a', args.a, '-d', '0', '-Y', args.Y, args.z], stdout=subprocess.PIPE)
 
-# main output example
-#Input 1 Voltage             =    202.644669 V
-#Input 1 Current             =      0.468859 A
-#Input 1 Power               =     95.011826 W
-#
-#Input 2 Voltage             =    201.899002 V
-#Input 2 Current             =      0.446302 A
-#Input 2 Power               =     90.108009 W
-#
-#Grid Voltage Reading        =    243.262344 V
-#Grid Current Reading        =      0.854146 A
-#Grid Power Reading          =    188.479767 W
-#Frequency Reading           =     50.050049 Hz.
-#
-#DC/AC Conversion Efficiency =         101.8 %
-#Inverter Temperature        =     29.993212 C
-#Booster Temperature         =     28.770926 C
-
-tmpoutput = """Input 1 Voltage             =    202.644669 V
+tmpoutput = """
+Input 1 Voltage             =    202.644669 V
 Input 1 Current             =      0.468859 A
 Input 1 Power               =     95.011826 W
 
@@ -44,8 +27,8 @@ Frequency Reading           =     50.050049 Hz.
 
 DC/AC Conversion Efficiency =         101.8 %
 Inverter Temperature        =     29.993212 C
-Booster Temperature         =     28.770926 C"""
-
+Booster Temperature         =     28.770926 C
+"""
 
 #for line in result.stdout.splitlines():
 for line in tmpoutput.splitlines():
